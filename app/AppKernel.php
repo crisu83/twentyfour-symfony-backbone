603f5af995ecd06095108390e36a4428532b8fc4
Crisu83 <ChristofferNiska@gmail.com>
2012-08-18 14:33:05 +0300

    Change app_dev for test deployment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Twentyfour\AppBundle\TwentyfourAppBundle(),
-            new BeSimple\Bundle\MustacheBundle\BeSimpleMustacheBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
